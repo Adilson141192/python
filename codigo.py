cb9f82f3f5309a6d00bc59ecf764a871560a6131
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-01-10 14:24:09 -0300

    011024 2

--- codigo.py
+++ codigo.py
@@ -17,5 +17,40 @@ pyautogui.write("minhasenha")
 pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(5)
-import pandas
 
+import pandas
+tabela = pandas.read_csv("produtos.csv")
+print(tabela)
+
+for linha in tabela.index:
+
+    pyautogui.click(x=580, y=391)
+
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(codigo)
+    pyautogui.press("tab")
+
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(marca)
+    pyautogui.press("tab")
+
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(tipo)
+    pyautogui.press("tab")
+
+    pyautogui.write(str(tabela.loc[linha, categoria]))
+    pyautogui.press("tab")
+
+    pyautogui.write(str(tabela.loc[linha, preco-unitario]))
+    pyautogui.press("tab")
+
+    pyautogui.write(str(tabela.loc[linha, custo]))
+    pyautogui.press("tab")
+
+    obs = tabela.loc[linha, "obs"]
+    if not pandas.isna(obs):
+        pyautogui.write(obs)
+    pyautogui.press("tab")
+
+    pyautogui.press("enter")  
+    pyautogui.scroll(2000)
